e5ac409d61293adbf95ccd63c410d3715acecf14
unknown <dethgrenade@gmail.com>
2020-10-26 16:38:55 -0700

    Added initial component tree to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { NavBar } from "./components/common/NavBar";
+import { ContentSection } from "./components/common/ContentSection";
+import {
+  aboutData,
+  projectData,
+  skillsData,
+  interestsData,
+  contactData,
+} from "./site-content";
 
 const App: React.FC = () => {
-  return <h1>Ready to Build!</h1>;
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <ContentSection data={aboutData} />
+      <ContentSection data={projectData} />
+      <ContentSection data={skillsData} />
+      <ContentSection data={interestsData} />
+      <ContentSection data={contactData} />
+
+      <form className="contact-form">
+        <label>
+          Name
+          <input />
+        </label>
+        <label>
+          Email
+          <input />
+        </label>
+        <label>
+          Phone Number
+          <input />
+        </label>
+        <label>
+          Message
+          <textarea />
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default App;
