a2a6ea0b4dbc168f09514c78f87f6ac227b1701e
unknown <dethgrenade@gmail.com>
2020-10-27 08:17:20 -0700

    updated ProjectCard to utilize new Project interface

--- src/components/common/ProjectCard.tsx
+++ src/components/common/ProjectCard.tsx
@@ -6,11 +6,12 @@ interface Props {
 }
 
 export const ProjectCard: React.FC<Props> = (props) => {
-  const { name, site_url, gitHub_url } = props.project;
+  const { name, site_url, gitHub_url, description } = props.project;
 
   return (
     <div className="project-card">
       <pre className="project-name">{name}</pre>
+      <p>{description}</p>
       <a href={site_url} target="_blank" rel="noreferrer">
         Visit Site
       </a>
