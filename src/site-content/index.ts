0f9918b3f8850dead19c0157f9c587da935ba333
unknown <dethgrenade@gmail.com>
2020-10-27 13:12:42 -0700

    removed unused imports

--- src/site-content/index.ts
+++ src/site-content/index.ts
@@ -3,13 +3,5 @@ import { projectData } from "./project-data";
 import { buildWeekData } from "./build-week-data";
 import { skillsData } from "./skills-data";
 import { interestsData } from "./interests-data";
-import { contactData } from "./contact-data";
 
-export {
-  aboutData,
-  projectData,
-  buildWeekData,
-  skillsData,
-  interestsData,
-  contactData,
-};
+export { aboutData, projectData, buildWeekData, skillsData, interestsData };
