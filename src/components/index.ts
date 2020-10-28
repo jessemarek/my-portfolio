f72845f7d041f84a5eeb0ddb5850875fa4350c5c
unknown <dethgrenade@gmail.com>
2020-10-28 14:01:51 -0700

    added font awesome imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import { Header } from "./Header";
 import { ContentSection } from "./ContentSection";
-import { ContactForm } from "./ContactForm";
+import { ContactSection } from "./ContactSection";
 import { Footer } from "./Footer";
 
-export { Header, ContentSection, ContactForm, Footer };
+export { Header, ContentSection, ContactSection, Footer };
