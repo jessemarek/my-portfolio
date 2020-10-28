ea6b92043ed7cd1cf185e323fe01cf1e15c00d2c
unknown <dethgrenade@gmail.com>
2020-10-28 08:09:11 -0700

    fixed order of links on nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,10 +11,10 @@ export const NavBar: React.FC = () => {
           <a href="#skills">Skills</a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
         </li>
       </ul>
     </nav>
