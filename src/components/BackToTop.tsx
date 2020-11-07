0f7719857f128bf17c77404579269215fb6c7b85
unknown <dethgrenade@gmail.com>
2020-11-07 10:42:43 -0800

    created back to top link component

---
+++ src/components/BackToTop.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+
+export const BackToTop: React.FC = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 280) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  });
+
+  return (
+    <>
+      {visible ? (
+        <Link
+          id="back-to-top"
+          to="top"
+          smooth={true}
+          href="#"
+          title="Back to Top"
+        >
+          <i className="far fa-caret-square-up"></i>
+        </Link>
+      ) : null}
+    </>
+  );
+};
