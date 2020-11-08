d7418f75698f4fc2f4aae9db32bff1e886c343a3
unknown <dethgrenade@gmail.com>
2020-11-08 15:13:08 -0800

    removed files for part of renaming refactor

--- src/interfaces/Content.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from "./Project";
-
-export interface Content {
-  id: string;
-  title: string;
-  description: string;
-  projects?: Project[];
-  skill_list?: string[];
-}
--- src/site-content/about-data.ts
+++
@@ -1,6 +0,0 @@
-export const aboutData = {
-  id: "about",
-  title: "About",
-  description:
-    "I am a Full Stack Web Developer recently graduated from Lambda School. I enjoy building cool projects and using my skills to help others see their dreams fullfilled. I am passionate about code and things relating to technology. I enjoy learning new things and solving challenging problems.",
-};
