4932a4fa4c3517482face1410c4f3871de43ac84
unknown <dethgrenade@gmail.com>
2020-11-08 15:43:35 -0800

    created initial InterestsContent component

---
+++ src/components/ContentSection/InterestsContent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {
+  content: Interests;
+}
+
+export interface Interests {
+  type: string;
+  details: string;
+}
+
+export const InterestsContent: React.FC<Props> = ({ content: { details } }) => {
+  return <></>;
+};
