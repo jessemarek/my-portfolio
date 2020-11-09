00bd6b0f6312ef9b537fd27b471473e305851afd
unknown <dethgrenade@gmail.com>
2020-11-09 15:31:41 -0800

    added a preload effect to Header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { NavBar } from "./NavBar";
 import { SectionContent } from "./ContentSection/ContentSection";
 
@@ -7,9 +7,17 @@ interface Props {
 }
 
 export const Header: React.FC<Props> = ({ links }) => {
+  const h1Ref = useRef<HTMLHeadingElement | null>(null);
+
+  useEffect(() => {
+    h1Ref.current && h1Ref.current.classList.remove("preload");
+  }, []);
+
   return (
     <header id="top">
-      <h1 className="header-title">Jesse Marek</h1>
+      <h1 ref={h1Ref} className="header-title preload">
+        Jesse Marek
+      </h1>
       <NavBar links={links} />
     </header>
   );
